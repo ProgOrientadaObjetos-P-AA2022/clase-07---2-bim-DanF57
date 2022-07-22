a43475ef84081656b91e041e27550aeb0debc626
Daniel Eduardo Flores <danser2002@hotmail.com>
2022-07-22 11:44:19 -0500

    Cambios Test

--- EjercicioJUnit/src/paquete3/Ciudad.java
+++ EjercicioJUnit/src/paquete3/Ciudad.java
@@ -10,6 +10,32 @@ package paquete3;
  * @author reroes
  */
 public class Ciudad {
+    private String nombres;
+    private int poblacion;
     
+    public void establecerNombres(String m){
+        nombres = m;
+    }
     
+    public void establecerPoblacion(int m){
+        poblacion = m;
+    }
+    
+    public String obtenerNombres(){
+        return nombres;
+    }
+    
+    public int obtenerPoblacion(){
+        return poblacion;
+    }
+    
+    @Override
+    public String toString(){
+        String reporte = String.format("Ciudad de Ecuador\n Nombre: %s\n\n\t"
+                + "Población: %d\n",
+                obtenerNombres(),
+                obtenerPoblacion()
+                );
+        return reporte;
+    }
 }
